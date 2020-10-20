8e3a537d08d9b797554d6912a0f480a16c4d3bb6
m-lada <maciej.lada@gmail.com>
2020-10-20 20:54:14 +0200

    3. Poprawa zadania

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,14 +4,10 @@ public class Calculator {
 
     Scanner scanner = new Scanner(System.in);
 
-    private int number;
-
     final private int upperCompartment = 200;
     final private int lowerCompartment = 100;
 
-    private boolean conditionDone = false;
-
-    private void numberCheck(int number) {
+    private boolean numberCheck(int number) {
         if (number < lowerCompartment) {
             System.out.println("Podana liczba jest za mała");
         } else if (number > upperCompartment) {
@@ -20,15 +16,17 @@ public class Calculator {
             System.out.println("Podana liczba nie jest podzielna przez 3");
         } else {
             System.out.println("Twoja liczba jest ok");
-            conditionDone = true;
+            return true;
         }
+        return false;
     }
 
     public void calculateNumber() {
+        boolean conditionDone;
         do {
             System.out.println("Podaj liczbę");
-            number = scanner.nextInt();
-            numberCheck(number);
+            int number = scanner.nextInt();
+            conditionDone = numberCheck(number);
         }
         while (
                 !conditionDone
